819472735edf043413ad1784a7bc6aa2d61fdbbc
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-10-11 03:28:30 +0200

    feature(api-client): add refit interface for user related actions

---
+++ Kinde.ApiClient/IKindeApiClient.User.cs
@@ -0,0 +1,34 @@
+using Kinde.ApiClient.Contracts.V1.Models;
+using Kinde.ApiClient.Contracts.V1.Requests;
+using Kinde.ApiClient.Contracts.V1.Responses;
+using Kinde.ApiClient.QueryParams;
+using Refit;
+
+namespace Kinde.ApiClient;
+
+public partial interface IKindeApiClient
+{
+    [Get("/api/v1/user")]
+    Task<User> GetUser(string id);
+
+    [Post("/api/v1/users/{user_id}/refresh_claims")]
+    Task<SuccessResponse> RefreshUserClaims([AliasAs("user_id")] string userId);
+
+    [Get("/api/v1/users")]
+    Task<UsersResponse> GetUsers(
+        [AliasAs("user_id")] string? userId = null,
+        [AliasAs("email")] string? email = null,
+        [AliasAs("sort")] UserSortingMethod? sortingMethod = null,
+        [AliasAs("page_size")] int? pageSize = null,
+        [AliasAs("next_token")] string? nextToken = null
+    );
+
+    [Post("/api/v1/user")]
+    Task<CreateUserResponse> CreateUser([Body] CreateUserRequest request);
+
+    [Patch("/api/v1/user")]
+    Task<UpdateUserResponse> UpdateUser(string id, [Body] UpdateUserRequest request);
+
+    [Delete("/api/v1/user")]
+    Task<SuccessResponse> DeleteUser(string id, [AliasAs("is_delete_profile")] bool shouldDeleteProfile = false);
+}
