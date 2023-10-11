cfe821f139d698d174314bcc223c45876529b15c
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-10-11 03:25:48 +0200

    feature: add sorting methods (enums) for request models

---
+++ Kinde.ApiClient/QueryParams/OrganizationSortingMethod.cs
@@ -0,0 +1,11 @@
+using System.Runtime.Serialization;
+
+namespace Kinde.ApiClient.QueryParams;
+
+public enum OrganizationSortingMethod
+{
+    [EnumMember(Value = "name_asc")] NameAscending,
+    [EnumMember(Value = "name_desc")] NameDescending,
+    [EnumMember(Value = "email_asc")] EmailAscending,
+    [EnumMember(Value = "email_desc")] EmailDescending
+}
