72af30dbaddb0394edfaae4e36bb07097729ffa1
Mateusz Różański <rozanski.mateusz00@gmail.com>
2023-12-08 01:56:48 +0100

    fix: breaking change to UsersResponse, due to the changes in the kinde management api contract.

--- KindeManagementApiClient/Contracts/V1/Models/User.cs
+++ KindeManagementApiClient/Contracts/V1/Models/User.cs
@@ -7,7 +7,7 @@ namespace KindeManagementApiClient.Contracts.V1.Models;
 /// </summary>
 /// <param name="Id">The user's identifier.</param>
 /// <param name="ProvidedId">The user's provided identifier.</param>
-/// <param name="PreferredEmail">The user's preferred email address.</param>
+/// <param name="Email">The user's email address.</param>
 /// <param name="LastName">The user's last name.</param>
 /// <param name="FirstName">The user's first name.</param>
 /// <param name="IsSuspended">Specifies whether the user account is suspended.</param>
@@ -22,8 +22,7 @@ public record User(
     [property: JsonPropertyName("id")] string Id,
     [property: JsonPropertyName("provided_id")]
     string ProvidedId,
-    [property: JsonPropertyName("preferred_email")]
-    string PreferredEmail,
+    [property: JsonPropertyName("email")] string Email,
     [property: JsonPropertyName("last_name")]
     string LastName,
     [property: JsonPropertyName("first_name")]
